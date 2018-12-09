eb9b74be94fbb755bf74f3b9456e227e7cb4f0cb
Dannel Albert <cuebix@gmail.com>
2018-12-09 00:43:16 -0500

    * Trying for separate conformance tests bundle

---
+++ src/conformance-tests.js
@@ -0,0 +1,4 @@
+export {
+  conformanceTests as actionProcessorConformanceTests
+} from './interfaces/ActionProcessorInterface.conformanceTests'
+export { conformanceTests as operativeConformanceTests } from './interfaces/OperativeInterface.conformanceTests'
