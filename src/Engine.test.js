c40dcfd05d426dc7c2b2a07eef4d4048eb1b1045
Dannel Albert <cuebix@gmail.com>
2018-12-08 19:40:36 -0500

    + Simple wiring test

--- src/Engine.test.js
+++ src/Engine.test.js
@@ -1,9 +1,23 @@
 /* global describe, test, expect */
 import { Engine } from './Engine'
+import { TestPresenter } from './adapters/TestPresenter'
 
 describe('Engine', () => {
   test('instantiation', () => {
     const result = new Engine()
     expect(result).toBeDefined()
+  })
+
+  test('wiring', () => {
+    const presenter = new TestPresenter()
+    const engine = new Engine(presenter)
+
+    expect(presenter.state).toBeUndefined()
+
+    engine.getModel().set(['foo', 'bar'], true)
+
+    engine.start()
+
+    expect(presenter.state).toEqual({ foo: { bar: true } })
   })
 })
