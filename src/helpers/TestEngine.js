feb9b7ce39c49343eaf01ce31ca935b300afe168
Dannel Albert <cuebix@gmail.com>
2018-12-22 00:46:27 -0600

    + data argument

--- src/helpers/TestEngine.js
+++ src/helpers/TestEngine.js
@@ -1,14 +1,15 @@
 import { TestPresenter } from '../adapters/TestPresenter'
 import { Engine } from '../Engine'
 
-export function TestEngine (opTree = {}) {
+export function TestEngine (opTree = {}, data = {}) {
   const engine = new Engine(new TestPresenter())
   const model = engine.getModel()
+  model.set([], data)
   model.setOpTree(opTree)
   engine.start()
   return engine
 }
 
-TestEngine.start = function (opTree) {
-  return new TestEngine(opTree)
+TestEngine.start = function (opTree, data) {
+  return new TestEngine(opTree, data)
 }
