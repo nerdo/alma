884ed89773ffb2f9082b9f5fee839d7d71d3af54
Dannel Albert <cuebix@gmail.com>
2018-12-22 18:29:32 -0600

    * Refactoring rollup configs into one

--- rollup.config.js
+++ rollup.config.js
@@ -4,26 +4,70 @@ import resolve from 'rollup-plugin-node-resolve'
 
 import pkg from './package.json'
 
-export default {
-  input: 'src/alma.js',
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
-      sourcemap: true
-    },
-    {
-      file: pkg.module,
-      format: 'es',
-      sourcemap: true
-    }
-  ],
-  plugins: [
-    babel({
-      exclude: 'node_modules/**',
-      runtimeHelpers: true
-    }),
-    resolve(),
-    commonjs()
-  ]
-}
+const plugins = [
+  babel({
+    exclude: 'node_modules/**',
+    runtimeHelpers: true
+  }),
+  resolve(),
+  commonjs()
+]
+
+export default [
+  // Main alma library.
+  {
+    input: 'src/alma.js',
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+        sourcemap: true
+      },
+      {
+        file: pkg.module,
+        format: 'es',
+        sourcemap: true
+      }
+    ],
+    plugins
+  },
+
+  // Useful, but not required operators.
+  {
+    input: 'src/ops.js',
+    output: [
+      {
+        file: 'dist/ops.js',
+        format: 'cjs',
+        sourcemap: true
+      },
+      {
+        file: 'dist/ops.es.js',
+        format: 'es',
+        sourcemap: true
+      }
+    ],
+    plugins
+  },
+
+  // Testing conveniences.
+  {
+    input: 'src/testing.js',
+    output: [
+      {
+        file: 'dist/testing.js',
+        format: 'cjs',
+        sourcemap: true
+      },
+      {
+        file: 'dist/testing.es.js',
+        format: 'es',
+        sourcemap: true
+      }
+    ],
+    external: [
+      'implements-interface'
+    ],
+    plugins
+  }
+]
