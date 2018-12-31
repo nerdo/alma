ac96d3471e120413fedf59988c484400f98f6c30
Dannel Albert <cuebix@gmail.com>
2018-12-31 01:23:30 -0600

    ^ integerSequence as non generator

--- src/helpers/integerSequence.js
+++ src/helpers/integerSequence.js
@@ -4,17 +4,29 @@
  * @param {number} startingAt - The number to start the sequence at
  * @param {boolean} [startOverWhenMaxIntegerIsReached=true]
  */
-export function * integerSequence (startingAt = 0, startOverWhenMaxIntegerIsReached = true) {
-  let integer = startingAt
-  while (true) {
-    if (integer === Number.MAX_SAFE_INTEGER) {
-      if (startOverWhenMaxIntegerIsReached) {
+export function integerSequence (startingAt = 0, startOverWhenMaxIntegerIsReached = true) {
+  let integer
+
+  return {
+    next () {
+      if (integer === void 0) {
         integer = startingAt
+      } else if (integer === Number.MAX_SAFE_INTEGER) {
+        if (startOverWhenMaxIntegerIsReached) {
+          integer = startingAt
+        } else {
+          throw new Error('Number.MAX_SAFE_INTEGER reached!')
+        }
       } else {
-        throw new Error('Number.MAX_SAFE_INTEGER reached!')
+        integer++
+      }
+
+      return {
+        done: startOverWhenMaxIntegerIsReached
+          ? false
+          : integer === Number.MAX_SAFE_INTEGER,
+        value: integer
       }
     }
-
-    yield integer++
   }
 }
